2a1f535a9105b2034253dc64bb8a1569a13d2d5f
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2015-08-06 18:06:34 +0200

    cleaned up custom.el

--- custom.el
+++ custom.el
@@ -3,24 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8874901e0011a7b07e546b65be1726c4cc3f35cf1a60f8805e6cb5bb59ba305c" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" default)))
- '(eshell-buffer-name "*eshell*")
  )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-dim-other-buffers-face ((t (:background "#eee8d5"))))
- '(flymake-errline ((t (:underline (:color "red" :style wave) :background nil :foreground nil :weight normal))))
- '(flymake-warnline ((t (:underline (:color "#DEBC52" :style wave) :background nil :foreground nil :weight normal))))
- '(flyspell-incorrect ((t (:underline (:color "red" :style wave) :weight normal :foreground nil))) t)
- '(font-lock-warning-face ((t (:foreground "#dc322f" :overline nil :underline nil))))
- '(git-gutter-fr:added ((t (:background "#859900" :foreground "#859900" :weight bold))))
- '(git-gutter-fr:deleted ((t (:background "#dc322f" :foreground "#dc322f" :weight bold))))
- '(git-gutter-fr:modified ((t (:background "#b58900" :foreground "#d33682" :weight bold))))
- '(linum ((t (:inherit (shadow default) :background "#EEE8D5" :foreground "#93A1A1"))))
- '(mode-line ((t (:background "#859900" :foreground "#394200"))))
-                                        ;'(mode-line-inactive ((t (:inherit mode-line :background "#fdf6e3" :foreground "#657b83" :box 1 :weight light))))
- '(powerline-active1 ((t (:background "#373f40" :foreground "#c8e4e8"))))
- '(powerline-active2 ((t (:background "#93a1a1" :foreground "#073642")))))
+ )
