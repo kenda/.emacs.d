53b394819925a9c66ff27d4ab0485bafb329e4c2
Marcus Nitzschke <nitzschke@gesundheitsforen.net>
2017-08-02 11:30:52 +0200

    get rid of cask + pallet - going to use use-package's :ensure

--- init.el
+++ init.el
@@ -1,13 +1,44 @@
-(require 'cl)
-(require 'org)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
-(require 'cask)
-(cask-initialize)
-(require 'pallet)
-(pallet-mode t)
+(require 'cl)
+
+; fetch the list of packages available
+(package-refresh-contents)
+
+; list the packages you want
+(setq package-list '( use-package
+                      org
+                      solarized-theme
+                      helm
+                      company
+                      flycheck
+                      fringe-helper
+                      git-gutter-fringe
+                      hydra
+                      keyfreq
+                      projectile
+                      yasnippet
+                      rainbow-delimiters
+                      aggressive-indent
+                      magit))
+
+; list the repositories containing them
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(require 'use-package)
+(require 'org)
 
 (when (file-exists-p "~/.emacs.d/local.conf.el")
   (load "~/.emacs.d/local.conf.el"))
 
-(require 'use-package)
 (org-babel-load-file "~/.emacs.d/config.org")
